2d6a8dc2ff7187af507e783ecf2a175817c407e0
Sero <serek9822@gmail.com>
2021-03-18 14:58:44 +0100

    OK

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void IfRandomWorks()
+        {
+                RandomNumberGenerator rng = new RandomNumberGenerator(1);
+
+                int x = rng.nextInt(1m,29m)
+
+                
+        }
+    }
+}
